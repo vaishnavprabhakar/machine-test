eb39456555f007b5890a5cd94a02a44b6b8999d2
Cybermate <vaishnavprabhakarkoo@gmail.com>
2023-11-21 19:17:27 +0530

    added something

--- app/admin.py
+++ app/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import CustomUser
 # Register your models here.
 
+
+from django.contrib.admin import AdminSite
+
+
+
+class DjangoAdinSite(AdminSite):
+    enable_nav_sidebar = True
+    model = CustomUser
+
+
 admin.site.register(CustomUser)
--- app/models.py
+++ app/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     username = models.CharField(unique=True,max_length=100)    
     email = models.EmailField(unique=True)
-
     
     def __str__(self):
         return self.email
